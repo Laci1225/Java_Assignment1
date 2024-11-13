a2ae31d255f04200b1523f634bb0dd8592867523
Nagy László Levente <nagylacika2002@gmail.com>
2024-11-13 21:43:00 +0100

    GoState implemented, yet runs to errors

--- src/main/java/org/example/GoState.java
+++ src/main/java/org/example/GoState.java
@@ -51,8 +51,7 @@ public class GoState implements Predicate<Point> {
         return Objects.hash(currentPlayer, Arrays.deepHashCode(board));
     }
 
-    public Point[] getNeighbors(){
-        Point point = new Point(1,1);
+    public Point[] getNeighbors(Point point){
         Point[] neighbours = new Point[]{
                 new Point(point.x + 1, point.y),
                 new Point(point.x, point.y + 1),
@@ -66,4 +65,66 @@ public class GoState implements Predicate<Point> {
     public boolean test(Point point) {
         return point.x >= 0 && point.x < board.length && point.y >= 0 && point.y < board.length;
     }
+
+    public Point[] getLiberties(Stone s, Point p, Set<Point> scanned){
+        Deque<Point> toScan = new LinkedList<>();
+        toScan.add(p);
+        Set<Point> liberties = new HashSet<>();
+        while (!toScan.isEmpty()) {
+            Point current = toScan.pop();
+            if (board[current.x][current.y].stone.equals(BoardSpace.EMPTY.stone)) {
+                liberties.add(current);
+            } else if (board[current.x][current.y].stone.equals(s)) {
+                scanned.add(current);
+                toScan.addAll(Arrays.asList(getNeighbors(current)));
+            }
+        }
+        return liberties.toArray(Point[]::new);
+    }
+    public void checkCaptured(Point p){
+        if (getLiberties(Stone.BLACK,p, new HashSet<>()).length == 0){
+            board[p.x][p.y] = BoardSpace.EMPTY;
+            whiteCaptured++;
+        }
+        if (getLiberties(Stone.WHITE,p, new HashSet<>()).length == 0){
+            board[p.x][p.y] = BoardSpace.EMPTY;
+            blackCaptured++;
+        }
+    }
+
+    public GoState placeStone(Point p){
+        var currentPlayer = this.currentPlayer;
+        board[p.x][p.y] = BoardSpace.fromStone(currentPlayer);
+        var neighbours = getNeighbors(p);
+        Arrays.stream(neighbours).forEach(this::checkCaptured);
+        return this;
+    }
+
+    public boolean isLegalMove(Point p){
+        var a =  this.test(p) && board[p.x][p.y].equals(BoardSpace.EMPTY);
+        var b = getLiberties(currentPlayer, p, new HashSet<>()).length == 1
+                && Arrays.stream(getNeighbors(p)).anyMatch(
+                        pr-> board[pr.x][pr.y].stone.equals(currentPlayer)
+        );
+        if (!a && !b) return false;
+        GoState newState = new GoState(this);
+        newState.placeStone(p);
+        return !previousStates.contains(newState);
+    }
+
+    public boolean makeMove(Point p){
+        // todo
+        if (!isLegalMove(p)) return false;
+        previousStates.add(new GoState(this));
+        placeStone(p);
+        currentPlayer = currentPlayer.opposite();
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Black Captured: " + blackCaptured + "\n" +
+                "White Captured: " + whiteCaptured;
+    }
 }
+
